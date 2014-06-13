2d71ea327a1ca37a4100370bc0c4f3b6918b3011
Thaddeus Ternes <thaddeus@bluetoo.co>
2014-06-13 14:45:12 -0500

    Initial Commit

---
+++ distnote/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  distnote
+//
+//  Created by Thaddeus on 6/13/14.
+//  Copyright (c) 2014 Bluetoo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
