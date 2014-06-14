4565b670aaaf80b5e9373c58df3dcee975fde0f9
Thaddeus Ternes <thaddeus@bluetoo.co>
2014-06-13 21:48:06 -0500

    Working app

--- distnote/main.m
+++ distnote/main.m
@@ -8,14 +8,43 @@
 
 #import <Foundation/Foundation.h>
 
+@interface BTOmniObserver : NSObject
+- (void)listenForEventName:(NSString *)name;
+@end
+
+@implementation BTOmniObserver
+
+- (void)listenForEventName:(NSString *)name
+{
+    NSDistributedNotificationCenter *nc = [NSDistributedNotificationCenter defaultCenter];
+    [nc addObserver:self selector:@selector(omniNotificationReceived:) name:name object:nil];
+}
+
+- (void)omniNotificationReceived:(NSNotification *)notification
+{
+    NSLog(@"%@", notification.name);
+    if(notification.object)
+        NSLog(@"object(%@): %@", NSStringFromClass([notification.object class]), notification.object);
+    if(notification.userInfo)
+        NSLog(@"%@", notification.userInfo);
+}
+
+@end
+
+#pragma mark -
+
 int main(int argc, const char * argv[])
 {
 
-    @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+    @autoreleasepool
+    {
+        __unused BTOmniObserver *omni = [[BTOmniObserver alloc] init];
+        if(argc > 1)
+            [omni listenForEventName:[NSString stringWithUTF8String:argv[1]]];
+        else
+            [omni listenForEventName:nil];
+
+        CFRunLoopRun();
     }
     return 0;
 }
